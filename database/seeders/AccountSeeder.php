36d1c24dcf2510a2f84a0208f953868e53c7fa5f
emilversiontwo <e39774338@gmail.com>
2026-06-05 14:12:14 +0300

    feat: Seeders and tests

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -10,22 +10,46 @@ class AccountSeeder extends Seeder
 {
     public function run(): void
     {
-        Account::query()->truncate();
+        $assetsType = AccountType::query()->where('name', 'Assets')->firstOrFail();
+        $revenueType = AccountType::query()->where('name', 'Revenue')->firstOrFail();
+        $expensesType = AccountType::query()->where('name', 'Expenses')->firstOrFail();
 
-        $account = new Account();
-        $account->accountType()->associate(AccountType::query()->firstOrFail());
-        $account->code = '60';
-        $account->name = 'SomeTestAccount';
-        $account->is_active = true;
+        $accounts = [
+            [
+                'account_type_id' => $assetsType->id,
+                'parent_account_id' => null,
+                'code' => '1000',
+                'name' => 'Cash',
+                'is_active' => true,
+            ],
+            [
+                'account_type_id' => $assetsType->id,
+                'parent_account_id' => null,
+                'code' => '1100',
+                'name' => 'Bank Account',
+                'is_active' => true,
+            ],
+            [
+                'account_type_id' => $revenueType->id,
+                'parent_account_id' => null,
+                'code' => '4000',
+                'name' => 'Sales Revenue',
+                'is_active' => true,
+            ],
+            [
+                'account_type_id' => $expensesType->id,
+                'parent_account_id' => null,
+                'code' => '5000',
+                'name' => 'Office Expenses',
+                'is_active' => true,
+            ],
+        ];
 
-        $account->save();
-
-        $account = new Account();
-        $account->accountType()->associate(AccountType::query()->firstOrFail());
-        $account->code = '70';
-        $account->name = 'SomeSecondTestAccount';
-        $account->is_active = false;
-
-        $account->save();
+        foreach ($accounts as $account) {
+            Account::query()->updateOrCreate(
+                ['code' => $account['code']],
+                $account
+            );
+        }
     }
 }
